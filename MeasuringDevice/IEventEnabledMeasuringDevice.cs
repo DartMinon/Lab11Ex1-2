f45d70da42e491cbc214a5a309933445b284a12f
Илья Абрамёнок <i.abramenok@slata.com>
2022-06-10 12:51:52 +0800

    Добавьте файлы проекта.

---
+++ MeasuringDevice/IEventEnabledMeasuringDevice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeasuringDevice
+{
+    
+
+    interface IEventEnabledMeasuringDevice : IMeasuringDevice
+    {
+        event EventHandler NewMeasurementTaken;
+        event HeartBeatEventHandler HeartBeat;
+        int HeartBeatInterval { get; }
+    }
+}
